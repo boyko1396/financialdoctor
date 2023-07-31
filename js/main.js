cafaf4425edf97e5041768f801d2674a36cf93f7
Vasya <boyko1396@gmail.com>
2023-07-31 17:07:41 +0300

    fix animated & scroll

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 // fullpage scroll init
 document.addEventListener('DOMContentLoaded', function() {
-    const layout = document.querySelector('body');
     const fullpageInstance = new fullpage('#fullpage', {
         css3: true,
         scrollingSpeed: 800,
@@ -9,11 +8,54 @@ document.addEventListener('DOMContentLoaded', function() {
         responsiveHeight: 330,
         controlArrows: false,
         fadingEffect: true,
-        anchors: ['intro', 'intro-hero', 'services', 'meta', 'appeal', 'footer']
+        recordHistory: false
+    });
+
+    // nav fullpagescroll
+    const scrollBtnHome = document.querySelector('.js-link-target');
+    const scrollBtnHome2 = document.querySelector('.js-link-target2');
+    const scrollBtnDown = document.querySelector('.main-hero__scroll-link-wrap');
+
+    scrollBtnDown.addEventListener('click', function(e) {
+        e.preventDefault();
+        fullpageInstance.moveSectionDown();
+    });
+
+    scrollBtnHome.addEventListener('click', function(e) {
+        e.preventDefault();
+        fullpageInstance.moveTo(1);
+    });
+
+    scrollBtnHome2.addEventListener('click', function(e) {
+        e.preventDefault();
+        fullpageInstance.moveTo(1);
+    });
+
+    // scroll animated for mobile
+    function handleIntersection(entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('is-visible');
+            } else {
+                entry.target.classList.remove('is-visible');
+            }
+        });
+    }
+
+    const observer = new IntersectionObserver(handleIntersection, {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.6
+    });
+
+    const elements = document.querySelectorAll('.js-scroll-trigger');
+    elements.forEach(element => {
+        observer.observe(element);
     });
 });
 
-//btn-toggle nav mobile
+
+// btn-toggle nav mobile
 const btnToggle = document.querySelector('.js-btn-toggle');
 const headerDropdown = document.querySelector('.header__dropdown');
 
